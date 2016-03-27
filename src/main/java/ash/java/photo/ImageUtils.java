9ef8789bb55829a1362432c81e27c3ec203368a3
Asher <asherjames.nz@gmail.com>
2016-03-28 12:24:32 +1300

    Finish scaleToThumbnail() in ImageUtils

--- src/main/java/ash/java/photo/ImageUtils.java
+++ src/main/java/ash/java/photo/ImageUtils.java
@@ -3,6 +3,7 @@ package ash.java.photo;
 import org.imgscalr.Scalr;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ImagingOpException;
 
 /**
  * Created by Asher on 20/03/2016.
@@ -11,7 +12,16 @@ public class ImageUtils {
 
     private ImageUtils() {}
 
-    public static void scaleToThumbnail(BufferedImage img, int maxWidth, int maxHeight) {
+    public static BufferedImage scaleToThumbnail(BufferedImage img, int maxWidth, int maxHeight) {
+        try {
+            if (img.getWidth() >= img.getHeight()) {
+                return Scalr.resize(img, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.FIT_TO_WIDTH, maxWidth, maxHeight);
+            } else {
+                return Scalr.resize(img, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.FIT_TO_HEIGHT, maxWidth, maxHeight);
+            }
+        } catch (IllegalArgumentException | ImagingOpException e) {
+            throw new PhotoWsException("Exception while creating thumbnail", e);
+        }
 
     }
 
